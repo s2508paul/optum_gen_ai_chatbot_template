bccca21fd041d429bbc3aaa56897eefff968c2c9
sumitpaul2508@gmail.com <sumitpaul2508@gmail.com>
2023-12-01 09:49:00 +0000

    🔁 Modified     - Dataset        - new_opensearch_sink        - content_vectorized     - Pipeline        - optum-pdf-data-vectorize        - data_vectorize
    ❇️ New
        - Job
           - test-optum-pipeline

--- pipelines/data_vectorize/code/data_vectorize/graph/write_vector_data.py
+++ pipelines/data_vectorize/code/data_vectorize/graph/write_vector_data.py
@@ -7,4 +7,12 @@ from data_vectorize.config.ConfigStore import *
 from data_vectorize.udfs.UDFs import *
 
 def write_vector_data(spark: SparkSession, in0: DataFrame):
+    from pyspark.sql.utils import AnalysisException
+
+    try:
+        desc_table = spark.sql("describe formatted `spark_catalog`.`default`.`pdf_bedrock_vectorized_text`")
+        table_exists = True
+    except AnalysisException as e:
+        table_exists = False
+
     in0.write.format("delta").mode("overwrite").saveAsTable("`spark_catalog`.`default`.`pdf_bedrock_vectorized_text`")

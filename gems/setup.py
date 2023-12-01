809632e50633dc29484685a617d130e20a871b05
sumitpaul2508@gmail.com <sumitpaul2508@gmail.com>
2023-12-01 09:49:50 +0000

    [Prophecy] Release version 1.5

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 packages_to_include = find_packages(exclude = ['test.*', 'test', 'test_manual'])
 setup(
     name = 'sumitpaulprophecyioteam_optumgenaichatbottemplate',
-    version = '1.3',
+    version = '1.5',
     packages = packages_to_include,
     description = '',
     install_requires = [],
